bed4fca4228095dd7030f73aa8ef181782a4a758
DipankarSethi3012 <dipankarsethi3012@gmail.com>
2025-03-01 08:47:45 +0530

    Easy Challange

---
+++ March/1-03-2025.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] == nums[i- 1]) {
+                nums[i - 1] *= 2;
+                nums[i] = 0;
+            }
+        }
+
+        int idx = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != 0) {
+                nums[idx++] = nums[i];
+            }
+        }
+
+        while(idx < n) {
+            nums[idx++] = 0;
+        }
+        
+
+        return nums;
+    }
+}
